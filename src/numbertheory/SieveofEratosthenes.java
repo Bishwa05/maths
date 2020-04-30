964ef3c1a7a9457be5453cd203f226fe5f98f7f4
Bishwa <bishwa.nayak@sap.com>
2020-05-01 00:52:31 +0530

    1st commit

--- src/numbertheory/SieveofEratosthenes.java
+++ src/numbertheory/SieveofEratosthenes.java
@@ -1,9 +1,36 @@
 package numbertheory;
 
+import java.util.List;
+
 /**
  *
  * algorithm for finding all the prime numbers in a segment [1;n] using O(nloglogn) operations.
  *
  */
 public class SieveofEratosthenes {
+
+    /**
+     * The runtime of this approach is O(n log logn).
+     *
+     */
+    public void findPrime(int n){
+
+        boolean[] isPrime = new boolean[n+1];
+
+        for(int i=2; i<isPrime.length; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i] && (long)i * i <= n) {
+                for (int j = i * i; j <= n; j += i)
+                    isPrime[j] = false;
+            }
+        }
+
+    }
+
+    public static void main(String arg[]){
+
+    }
 }
